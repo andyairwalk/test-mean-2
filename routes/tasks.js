412c1f24602e53da0c577af2e7b4934b84a8a539
Andrea <andrea@andreas-mini.home>
2017-05-01 11:20:54 +0100

    get single task

--- routes/tasks.js
+++ routes/tasks.js
@@ -3,12 +3,23 @@ var router = express.Router();
 var mongojs = require('mongojs');
 var db = mongojs('mongodb://test:test@ds127531.mlab.com:27531/products', ['tasks']);
 
+//get all tasks
 router.get('/tasks', function(req, res, next){
 	db.tasks.find(function(err, tasks){
 		if(err) {
 			res.send(err);
 		}
 		res.json(tasks);
+	});
+});
+
+//get single task
+router.get('/task/:id', function(req, res, next){
+	db.tasks.findOne({_id: mongojs.ObjectId(req.params.id)}, function(err, task){
+		if(err) {
+			res.send(err);
+		}
+		res.json(task);
 	});
 });
 
