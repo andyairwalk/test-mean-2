e6b9827252fa807c2a78233b932031309b8cb4f2
Andrea <andrea@andreas-mini.home>
2017-05-01 11:15:35 +0100

    db works

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://test:test@ds127531.mlab.com:27531/products', ['tasks']);
 
 router.get('/tasks', function(req, res, next){
-	res.send('tasks page');
+	db.tasks.find(function(err, tasks){
+		if(err) {
+			res.send(err);
+		}
+		res.json(tasks);
+	});
 });
 
 module.exports = router;
